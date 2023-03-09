aa71cf59d5c441df823d7fdde6d9b56292496c77
benanakkaya <ahmetbenanakkaya@gmail.com>
2023-03-09 16:40:15 +0300

    fix

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './input.css';
+import '../dist/output.css'
 import store from './redux/store';
 import { Provider } from 'react-redux'
 
